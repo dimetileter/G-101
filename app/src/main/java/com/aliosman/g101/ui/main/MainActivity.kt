992e5ccd3037caab4a1e4dd344c5012e863e7ef8
ali osman <aliosmankaraogul4334@gmail.com>
2025-09-15 23:47:14 +0300

    - added resource files (color-night, string-en/tr, style) - added onboarding fragments - added icon set and fonts - added DPI-specific drawable resources

---
+++ app/src/main/java/com/aliosman/g101/ui/main/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.aliosman.g101.ui.main
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.aliosman.g101.R
+import com.aliosman.g101.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Binding bağla
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, 0, systemBars.right, 0)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/aliosman/g101/ui/onBoarding/OnBoardingActivity.kt
@@ -0,0 +1,60 @@
+package com.aliosman.g101.ui.onBoarding
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.aliosman.g101.R
+import com.aliosman.g101.databinding.OnBoardingActivityBinding
+import com.aliosman.g101.ui.main.MainActivity
+import kotlin.jvm.java
+
+class OnBoardingActivity : AppCompatActivity() {
+
+    private lateinit var binding: OnBoardingActivityBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        // Binding
+        binding = OnBoardingActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, 0, systemBars.right, 0)
+            insets
+        }
+
+        // NavHostFragment üzerinden navController al
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+        navController = navHostFragment.navController
+
+        // Butonu ayarla
+        val nextButton = binding.btnSegmentedNextButton.btnSegmentedNextButton
+        nextButton.isSelected = true
+
+        nextButton.setOnClickListener {
+            when (navController.currentDestination?.id) {
+                R.id.introFragment1 -> {
+                    navController.navigate(R.id.action_introFragment1_to_introFragment2)
+                }
+                R.id.introFragment2 -> {
+                    navController.navigate(R.id.action_introFragment2_to_introFragment3)
+                }
+                R.id.introFragment3 -> {
+                    // 3. fragmentten sonra başka activity aç
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }
+        }
+    }
+}
